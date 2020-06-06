abaedf70f2643d3cf6a3f06dfcd9969e5c2541a4
hlnmargary <margary.helene@hetic.net>
2020-06-06 13:07:44 +0200

    start loader

--- src/partials/loader/loader.js
+++ src/partials/loader/loader.js
@@ -1,35 +1,47 @@
 import './loader.scss';
 
+let loader = document.querySelector('#loader');
+let loaderContainer = document.querySelector('.loaderContainer');
+//loader
+
+
 window.onload = function () {
 
-  let loader = document.querySelector('#loader');
-  let strings = loader.dataset.strings.split(',');
-  let counter = 0;
+  var strings = loader.dataset.strings.split(',');
+  var counter = 0;
+  // setTimeout(() => {
+  //   loader.classList.add('hideLoader');
+  // }, 1000);
+
 
   setInterval(function () {
-    let logoTitle = strings[counter];
-    if (counter < strings) {
+
+    var logoRandom = '';
+    var possible = "-+*/|}{[]~\\\":;?/.><=+-_)(*&^%$#@!)}";
+    var logoTitle = strings[counter];
+    if (counter < 4) {
       counter++;
+      generateRandomTitle();
+    } else {
+      loaderContainer.style.display = "none";
     }
 
-    let logoRandom = '';
-    let possible = "?=+*&^%$#@!)}";
 
     function generateRandomTitle(i, logoRandom) {
       loader.classList.add('glitch');
       setTimeout(function () {
         loader.innerHTML = logoRandom;
         loader.dataset.text = logoRandom;
-      }, i * 55);
+      }, i * 80);
     }
 
-    for (let i = 0; i < logoTitle.length + 1; i++) {
+    for (var i = 0; i < logoTitle.length + 1; i++) {
       logoRandom = logoTitle.substr(0, i);
-      for (let j = i; j < logoTitle.length; j++) {
+      for (var j = i; j < logoTitle.length; j++) {
         logoRandom += possible.charAt(Math.floor(Math.random() * possible.length));
       }
       generateRandomTitle(i, logoRandom);
       logoRandom = '';
     }
   }, 2100);
-};
+}
