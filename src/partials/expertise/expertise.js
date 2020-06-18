2bf6388270ab4d5a9a12003dd145a9ed8d74928b
hlnmargary <margary.helene@hetic.net>
2020-06-18 22:00:49 +0200

    change desgin

--- src/partials/expertise/expertise.js
+++ src/partials/expertise/expertise.js
@@ -1,140 +1,18 @@
 import './expertise.scss';
 import { TweenMax, Linear } from "gsap";
 
-const blocks = [...document.querySelectorAll('.expertise-block')];
-const menus = [...document.querySelectorAll('.dropDownMenu')];
-const cards = [...document.querySelectorAll('.cardProject')];
-const circleDev = document.querySelector('.svg-element-dev');
-const circleUx = document.querySelector('.svg-element-ux');
 
-const getParent = (el) => {
-    while (!el.dataset.expertise) {
-        el = el.parentNode;
-    }
-    return el;
-}
+let circlePath = document.getElementById("circlePath");
+let lengthCirclePath = circlePath.getTotalLength();
+let circle = document.getElementById("circle");
+let textCurved = document.getElementById("textCurved");
 
-const handleHover = el => {
-    if (!el.dataset.expertise) {
-        el = getParent(el)
-    }
-    blocks.forEach(block => {
-        if (el.dataset.expertise === block.dataset.expertise) {
-            block.classList.add('large');
-            if (block.classList.contains('little')) {
-                block.classList.remove('little');
-            }
-        }
-        else if (el.dataset.expertise !== block.dataset.expertise) {
-            block.classList.add('little');
-            if (block.classList.contains('large')) {
-                block.classList.remove('large');
-
-            }
-        }
-        cards.forEach(card => {
-            if (block.dataset.expertise === card.dataset.card) {
-                if (block.classList.contains('little')) {
-                    card.classList.add('hided');
-                }
-                else if (block.classList.contains('large')) {
-                    card.classList.remove('hided');
-                }
-            }
-        });
-        menus.forEach(menu => {
-            if (block.dataset.expertise === menu.dataset.menu) {
-                if (block.classList.contains('little')) {
-                    menu.classList.add('hided');
-                }
-                else if (block.classList.contains('large')) {
-                    menu.classList.remove('hided');
-                }
-            }
-        });
-        if (el.dataset.expertise === 'ux') {
-            circleUx.classList.add('rotate');
-        }
-        else if (circleUx.classList.contains('rotate')) {
-            circleUx.classList.remove('rotate');
-        }
-        if (el.dataset.expertise === 'dev') {
-            circleDev.classList.add('rotate');
-        }
-        else if (circleDev.classList.contains('rotate')) {
-            circleDev.classList.remove('rotate');
-        }
-
-
-    });
-    getRotationAngle(circleUx);
-
-};
-
-function getRotationAngle(target) {
-    const obj = window.getComputedStyle(target, null);
-    const matrix = obj.getPropertyValue('-webkit-transform') ||
-        obj.getPropertyValue('-moz-transform') ||
-        obj.getPropertyValue('-ms-transform') ||
-        obj.getPropertyValue('-o-transform') ||
-        obj.getPropertyValue('transform');
-
-    let angle = 0;
-    console.log(obj);
-
-
-    if (matrix !== 'none') {
-        const values = matrix.split('(')[1].split(')')[0].split(',');
-        console.log(values);
-
-        const a = values[0];
-        const b = values[1];
-        var radians = Math.atan2(b, a);
-        if (radians < 0) {
-            radians += (2 * Math.PI);
-        }
-        angle = Math.round(radians * (180 / Math.PI));
-        // console.log(angle);
-    }
-
-    return (angle < 0) ? angle += 360 : angle;
-}
-
-
-
-
-blocks.forEach(block => {
-    block.addEventListener('mouseover', function (e) {
-        handleHover(e.target);
-    });
+document.getElementById("pathTextCurved").setAttribute(
+    "textLength", lengthCirclePath - 20);
+const hover = TweenMax.to(textCurved, 40, {
+    transformOrigin: "50% 50%",
+    rotation: "-360",
+    repeat: -1,
+    ease: Linear.easeNone
 });
 
-
-
-const circlePath = document.getElementById("circlePath");
-const lengthCirclePath = circlePath.getTotalLength();
-const pathTextCurved = document.querySelector(".pathTextCurved");
-let circlePath1 = document.getElementById("circlePath1");
-let lengthCirclePath1 = circlePath1.getTotalLength();
-let circle1 = document.getElementById("circle1");
-let textCurved1 = document.getElementById("textCurved1");
-
-pathTextCurved.setAttribute("textLength", lengthCirclePath - 20);
-pathTextCurved1.setAttribute("textLength", lengthCirclePath1 - 20);
-
-
-const subnavTitles = [...document.querySelectorAll('.subnavigation-name')];
-const subnavParts = [...document.querySelectorAll('.subnavigation-part')];
-
-
-subnavTitles.forEach(subnavTitle => {
-    subnavTitle.addEventListener('click', function (e) {
-        subnavParts.forEach(subnavPart => {
-            if (subnavTitle.dataset.nav == subnavPart.dataset.part) {
-                subnavPart.classList.add('displayed');
-            } else if (subnavPart.classList.contains('displayed')) {
-                subnavPart.classList.remove('displayed');
-            }
-        });
-    });
-});
--- src/partials/projects/projects.js
+++ src/partials/projects/projects.js
@@ -1,68 +1,87 @@
 import './projects.scss';
 import datas from '../../assets/datas/data.json';
+import { TweenMax, Linear } from "gsap";
 
 const getDataAccordingToParams = (param) => datas.filter(el => el[param] == true);
 
 /*
 * Get params from URL
 */
-const params = new URLSearchParams(document.location.search.substring(1));
-let dataFiltered = null;
-for (const key of params.keys()) {
-    switch (key) {
-        case 'dev':
-            dataFiltered = getDataAccordingToParams(key);
-            break;
-        case 'design':
-            dataFiltered = getDataAccordingToParams(key);
-            break;
-        default:
-            console.log(`Sorry, the case is not handled`);
+// const params = new URLSearchParams(document.location.search.substring(1));
+// let dataFiltered = null;
+// for (const key of params.keys()) {
+//     switch (key) {
+//         case 'dev':
+//             dataFiltered = getDataAccordingToParams(key);
+//             break;
+//         case 'design':
+//             dataFiltered = getDataAccordingToParams(key);
+//             break;
+//         default:
+//             console.log(`Sorry, the case is not handled`);
+//     }
+// }
+
+
+const circles = [...document.querySelectorAll('.circle')];
+const navs = [...document.querySelectorAll('.nav-item')];
+const project = document.querySelectorAll('.project');
+console.log(project.dataset);
+
+const handleClick = el => {
+    if (el.dataset.number === project.dataset.number) {
+        project.classList.add('active');
+        if (project.classList.contains('active')) {
+            project.classList.remove('active');
+        }
     }
-}
+    else if (el.dataset.number !== project.dataset.number) {
+        project.classList.add('active');
+        if (project.classList.contains('large')) {
+            project.classList.remove('large');
 
-const projects = document.querySelector('.project');
-const wrapper = document.querySelector('.wrapper');
-projects.setAttribute('id', 'project-1');
-for (let i = 0; i < dataFiltered.length - 1; i++) {
-    const clone = projects.cloneNode(true);
-    clone.setAttribute('id', `project-${i + 2}`);
-    wrapper.appendChild(clone);
-}
-
-const image = document.querySelectorAll('.project_visual-image');
-const text = document.querySelectorAll('.project_presentation-text');
-const title = document.querySelectorAll('.project_presentation-title');
-const subtitle = document.querySelectorAll('.project_presentation-subtitle');
-const id = document.querySelectorAll('.project_card-number');
-
-const templateToFill = [];
-
-
-const createObj = () => {
-    image.forEach((el, i) => {
-        const myObj = new Object();
-        myObj.image = image[i];
-        myObj.text = text[i];
-        myObj.title = title[i];
-        myObj.subtitle = subtitle[i];
-        myObj.id = id[i];
-        templateToFill.push(myObj);
-    })
+        }
+    }
 };
 
-createObj();
 
-const populateData = (id) => {
-    templateToFill[id].image.style.backgroundImage = `url(assets/${datas[id].image})`;
-    templateToFill[id].text.textContent = dataFiltered[id].description;
-    templateToFill[id].title.textContent = dataFiltered[id].name;
-    templateToFill[id].subtitle.textContent = dataFiltered[id].subtitle;
-    templateToFill[id].id.textContent = dataFiltered[id].number;
-};
+// const handleClick = el => {
+//     if (el.closest('svg').classList.contains('rotate')) {
+//         el.closest('svg').classList.remove('rotate');
+//     } else {
+//         el.closest('svg').classList.add('rotate');
+//     }
+// };
 
-dataFiltered.forEach((el, i) => {
-    populateData(i);
+navs.forEach(nav => {
+    nav.addEventListener('click', function (e) {
+        handleClick(e.target);
+        console.log('clicked');
+    });
 });
 
+// const circlePath = document.getElementById("circlePath");
+// const lengthCirclePath = circlePath.getTotalLength();
+// const pathTextCurved = document.querySelector(".pathTextCurved");
+// let circlePath1 = document.getElementById("circlePath1");
+// let lengthCirclePath1 = circlePath1.getTotalLength();
+// let circle1 = document.getElementById("circle1");
+// let textCurved1 = document.getElementById("textCurved1");
+
+// pathTextCurved.setAttribute("textLength", lengthCirclePath - 20);
+// pathTextCurved1.setAttribute("textLength", lengthCirclePath1 - 20);
+
+// let circlePath = document.getElementById("circlePath");
+// let lengthCirclePath = circlePath.getTotalLength();
+// let circle = document.getElementById("circle");
+// let textCurved = document.getElementById("textCurved");
+
+// document.getElementById("pathTextCurved").setAttribute(
+//     "textLength", lengthCirclePath - 20);
+// const hover = TweenMax.to(textCurved, 40, {
+//     transformOrigin: "50% 50%",
+//     rotation: "-360",
+//     repeat: -1,
+//     ease: Linear.easeNone
+// });
 
